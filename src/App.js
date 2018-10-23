77dc6fc874ad03146f2ef230d4551a4491c2045c
RyanPWalker <RyanPrestonWalker@gmail.com>
2018-10-22 23:09:42 -0600

    a start.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import * as posenet from '@tensorflow-models/posenet';
+import trumpImg from './trump.jpg';
 import './App.css';
 
 class App extends Component {
+  async componentDidMount() {
+    const scaleFactor = 0.50;
+    const flipHorizontal = false;
+    const outputStride = 16;
+    const imageElement = document.getElementById('trump');
+    // load the posenet model
+    const net = await posenet.load();
+    const pose = await net.estimateSinglePose(imageElement, scaleFactor, flipHorizontal, outputStride);
+    console.log('pose', pose); // spits out array of joint positions
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,13 +26,14 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="ryanwalker.party"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn to Party
           </a>
         </header>
+        <img id="trump" src={trumpImg} alt="trump" />
       </div>
     );
   }
